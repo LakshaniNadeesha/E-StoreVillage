f2d564c8471ba054b3f883fb5693f44cc1c1f8f0
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2022-09-02 22:29:58 +0530

    Add Promotion Accept and Reject Js and CSS Files

---
+++ src/pages/ManagerPages/PromotionRequest/PromotionRequestReject.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./PromotionAcceptReject.css";
+import {useNavigate} from "react-router-dom";
+
+const PromotionRequestReject = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="pro-accept-main-container">
+            <div className="pro-accept-row-container">
+                <p className="verify-p">Are you sure you want to accept the promotion request?</p>
+                <div className="pro-manager-request-respond-option">
+                    <button className="pro-manager-reject">Reject</button>
+                    <button className="pro-manager-cancel" onClick={() => navigate("/promotion_request")}>Cancel</button>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default PromotionRequestReject;
