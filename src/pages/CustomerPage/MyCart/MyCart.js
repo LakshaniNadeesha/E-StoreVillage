ea7b606e08a4fc0d0a1a1a476ad73ca14b70c07e
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2022-09-09 17:08:22 +0530

    Update Customer MyCart Files

--- src/pages/CustomerPage/MyCart/MyCart.js
+++ src/pages/CustomerPage/MyCart/MyCart.js
@@ -8,85 +8,31 @@ import {AiOutlineShoppingCart} from "react-icons/ai";
 import Rating from "react-rating";
 import {IoStarOutline} from "react-icons/io5";
 import {GrStar} from "react-icons/gr";
+import Giveaways from "../EventsPage/Giveaways/Giveaways";
+import CartProducts from "./CartProducts";
 
 
 const MyCart = () => {
-  return (
-    <div className="mycart-main-container">
+    const productList = [{}, {},{}];
+
+    return (
+    <div className="my-cart-main-container">
       <div className="my-cart-row">
-          <MdCheckBoxOutlineBlank size={32} />
-          <span className="my-cart-total-amout">Select All</span>
-        <span className="my-cart-total-amout">
-          Total Amont - <h3> Rs. 1200</h3>
+          <input type="checkbox" className="cart-checkbox"/>
+          <span className="my-cart-total-amount">Select All</span>
+        <span className="my-cart-total-amount">
+          Total Amount - <h3 className="total-price"> Rs. 1200</h3>
         </span>
 
         <button className="my-cart-checkout-btn">Checkout</button>
       </div>
-        <div className="add-cart">
-        <div className="add-products-cart">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <img src={Chain} className="image-container" />
-            </div>
-            <p className="description">24 in stocks for 3 variants </p>
-            <h5 className="product-name">Handmade Necklace </h5>
-            <div className="price-container">
-                <span className="price">LKR 400.00</span>
-                <span className="grand-total">LKR 1250.00</span>
-            </div>
-            <div className="btn-row1">
-                <button className="yellow-btn1">
-                    Buy Now
-                </button>
-                <button className="yellow-btn1">
-                    Visit The Shop
-                </button>
+            <div className="cart-products-row-container">
+                <h3 className="my-cart-products-title">Products</h3>
             </div>
 
-            </div>
-
-            <div className="add-products-cart">
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <img src={Chain} className="image-container" />
-                </div>
-                <p className="description">24 in stocks for 3 variants </p>
-                <h5 className="product-name">Handmade Necklace </h5>
-                <div className="price-container">
-                    <span className="price">LKR 400.00</span>
-                    <span className="grand-total">LKR 1250.00</span>
-                </div>
-                <div className="btn-row1">
-                    <button className="yellow-btn1">
-                        Buy Now
-                    </button>
-                    <button className="yellow-btn1">
-                        Visit The Shop
-                    </button>
-                </div>
-            </div>
-
-
-            <div className="add-products-cart">
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <img src={Chain} className="image-container" />
-                </div>
-                <p className="description">24 in stocks for 3 variants </p>
-                <h5 className="product-name">Handmade Necklace </h5>
-                <div className="price-container">
-                    <span className="price">LKR 400.00</span>
-                    <span className="grand-total">LKR 1250.00</span>
-                </div>
-                <div className="btn-row1">
-                    <button className="yellow-btn1">
-                        Buy Now
-                    </button>
-                    <button className="yellow-btn1">
-                        Visit The Shop
-                    </button>
-                </div>
-            </div>
-
-
-            </div>
+            {productList.map((item, index) => (
+                <CartProducts key={index} />
+            ))}
 
     </div>
   );
